7583067235c0212837a1a04e4745e15739252dd6
noQils <daffaaqilmahmud@gmail.com>
2024-09-23 19:28:31 +0700

    first push assignment 4

--- sigmart/settings.py
+++ sigmart/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z53kqgra$l^%f#qqmt%pqewf&^_1)3i$8n9%xlnxz%y758^j__'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "daffa-aqil31-sigmart.pbp.cs.ui.ac.id"]
 
